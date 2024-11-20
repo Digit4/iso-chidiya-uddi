2bfcff273873953892137e6594c09b5f9be24fce
Digit4 <dhaval.pantojee@gmail.com>
2024-11-20 15:10:57 +0530

    missed push

--- frontend/src/components/ChidiyaDisplayBox.jsx
+++ frontend/src/components/ChidiyaDisplayBox.jsx
@@ -1,5 +1,18 @@
-const ChidiyaDisplayBox = ({ children }) => {
-    return <>{children}</>;
+import { useEffect } from 'react';
+import Input from './Input';
+
+const ChidiyaDisplayBox = ({ children, udd }) => {
+    useEffect(() => {
+        console.log('udd', udd);
+    }, [udd]);
+    return (
+        <>
+            {children}
+            <div className='text-center'>
+                <Input type='checkbox' disabled checked={udd} />
+            </div>
+        </>
+    );
 };
 
 export default ChidiyaDisplayBox;
